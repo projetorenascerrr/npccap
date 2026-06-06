018d7a587f495568612d0cc3492700eea7b662cd
RogerioRegis <rog_reg@hotmail.com>
2026-06-06 12:39:15 -0400

    chore: atualiza ajustes gerais

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'NPCCAP')</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('icon/favicon-16x16.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('icon/favicon-32x32.png') }}" type="image/x-icon">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://kit.fontawesome.com/a81368914c.js" crossorigin="anonymous"></script>
